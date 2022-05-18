b219f4c68f8da55a84d7b62ec861db72499dbef3
Tianjian <mail@isdtj.com>
2022-05-18 23:16:26 +0800

    增加测试组件

---
+++ src/components/list.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>List</h1>
+    <ul>
+      <li>A1</li>
+      <li>A2</li>
+    </ul>
+  </section>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
